b20f982ee7ba51b501600a27a5f349cba1a7d1f5
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-30 20:05:53 +0200

    Store

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import reducers from './reducers';
+
+const loggerMiddleware = createLogger();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
+);
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    reducers,
+    preloadedState,
+    enhancer
+  );
+}
