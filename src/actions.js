3b72f6b2fd56da91b6d1c9a6840e693d22ab07f0
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-01 12:41:45 +0200

    Preparing items

--- src/actions.js
+++ src/actions.js
@@ -45,3 +45,64 @@ function fetchAllItems(searchStr) {
       ))
   }
 }
+
+function prepareItems(array= {
+  let combined = [];
+  array.forEach((item) => {
+    combined = combined.concat(item.results);
+  });
+
+  return combined.map((item) => {
+    if (Object.hasOwnProperty.call(item, 'episode_id')) {
+      return {
+        type: 'film',
+        name: item.title,
+        episode_id: item.episode_id,
+        director: item.director,
+        producer: item.producer,
+        release_date: item.release_date,
+      };
+    } else if (Object.hasOwnProperty.call(item, 'model')) {
+      return {
+        type: 'starship',
+        name: item.name,
+        model: item.model,
+        hyperdrive_rating: item.hyperdrive_rating,
+        manufacturer: item.manufacturer,
+      };
+    } else if (Object.hasOwnProperty.call(item, 'classification')) {
+      return {
+        type: 'species',
+        name: item.name,
+        classification: item.classification,
+        designation: item.designation,
+        language: item.language,
+      };
+    } else if (Object.hasOwnProperty.call(item, 'orbital_period')) {
+      return {
+        type: 'planet',
+        name: item.name,
+        gravity: item.gravity,
+        terrain: item.terrain,
+        population: item.population,
+      };
+    }
+    return {
+      type: 'person',
+      name: item.name,
+      gender: item.gender,
+      height: item.height,
+      mass: item.mass,
+    };
+  }).sort(compareNames);
+})
+
+function compareNames(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
