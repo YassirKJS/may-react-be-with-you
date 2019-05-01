ef639106db926b80e7fe60c48a6b7cab9f057915
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-01 11:36:27 +0200

    Actions 1

--- src/actions.js
+++ src/actions.js
@@ -4,3 +4,25 @@ import {
   RECEIVE_ITEMS
 } from './constants.js';
 
+function changeSearchFieldAction(searchStr) {
+  return {
+    type: CHANGE_SEARCH_FIELD,
+    searchStr
+  };
+}
+
+function requestItemsAction(searchStr) {
+  return {
+    type: REQUEST_ITEMS,
+    searchStr
+  };
+}
+
+function receiveItemsAction(searchStr, items) {
+  return {
+    type: RRECEIVE_ITEMS,
+    searchStr,
+    items,
+    receivedAt: Date.now()
+  };
+}
