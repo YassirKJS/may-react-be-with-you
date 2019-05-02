16bec8e2bd72edf0dedd8ccadd2f359610a29b2f
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-02 20:47:51 +0200

    film

---
+++ src/components/Results/film.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+onst Film = props => (
+  <li className="results__item">
+    <h3 className="results__title">
+      {props.item.name}
+      <span> (Episode {props.item.episode_id})</span>
+    </h3>
+    <div
+      className="results__bg"
+      id={props.item.type}
+      dangerouslySetInnerHTML={{ __html: icon }}
+    />
+    <ul className="results__info">
+      <li>Released <span>{props.item.release_date}</span> </li>
+      <li>Directed by <span>{props.item.director}</span> </li>
+      <li>Produced by <span>{props.item.producer}</span> </li>
+    </ul>
+  </li>
+);
+
+export default Film;
