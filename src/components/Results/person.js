d7907b9906af5b3c0e231de4eeb6180c0824fff6
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-02 20:50:21 +0200

    person

---
+++ src/components/Results/person.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Person = props => (
+  <li className="results__item">
+    <h3 className="results__title">{props.item.name}</h3>
+    <div
+      className="results__bg"
+      id={props.item.type}
+      dangerouslySetInnerHTML={{ __html: icon }}
+    />
+    <ul className="results__info">
+      <li>Gender <span>{props.item.gender}</span></li>
+      <li>Height <span>{props.item.height}cm</span> </li>
+      <li>Weight <span>{props.item.mass}kg</span> </li>
+    </ul>
+  </li>
+);
+
+export default Person;
