31290b10c8735be0948acdde880e1f5e4213caf4
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-03 21:35:04 +0200

    species

---
+++ src/components/Results/species.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import icon from './images/wookie.svg';
+
+const Film = props => (
+  <li className="results__item">
+    <h3 className="results__title">
+      {props.item.name}
+    </h3>
+    <div
+      className="results__bg"
+      id={props.item.type}
+      dangerouslySetInnerHTML={{ __html: icon }}
+    />
+    <ul className="results__info">
+      <li>Designation <span>{props.item.designation}</span> </li>
+      <li>Classification <span>{props.item.classification}</span> </li>
+      <li>Language <span>{props.item.language}</span> </li>
+    </ul>
+  </li>
+);
+
+export default Film;
