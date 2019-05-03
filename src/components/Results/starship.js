297d15d6c6b4cde32a3da3b9fb25acf898491b5a
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-03 21:35:29 +0200

    starship

---
+++ src/components/Results/starship.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import icon from './images/starship.svg';
+
+const Film = props => (
+  <li className="results__item">
+    <h3 className="results__title">
+      {props.item.name}
+    </h3>
+    <div
+      className="results__bg"
+      id={props.item.type}
+      dangerouslySetInnerHTML={{ __html: icon }}
+    />
+    <ul className="results__info">
+      <li>Model <span>{props.item.model}</span> </li>
+      <li>Manufacturer <span>{props.item.manufacturer}</span> </li>
+      <li>Hyperdrive Rating <span>{props.item.hyperdrive_rating}</span> </li>
+    </ul>
+  </li>
+);
+
+export default Film;
